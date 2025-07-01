279c8e731a3e756361f87368309a2e41e4a4e218
KR0079384 <karthiraman2006@gmail.com>
2025-07-01 11:52:23 +0530

    Just In Time⚡

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -5,3 +5,4 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+//you cant be something mean to anything but nothing like what it sort of looks like
